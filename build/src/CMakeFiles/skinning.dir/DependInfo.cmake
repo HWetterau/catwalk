246e5b1bcc1fa46470947ea6d8f8b66695de7d9f
HWetterau <wetterau.hannah@gmail.com>
2019-03-25 12:14:36 -0500

    assets

---
+++ build/src/CMakeFiles/skinning.dir/DependInfo.cmake
@@ -0,0 +1,42 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/u/hwett/Desktop/graphics/catwalk/src/animation_loader_saver.cc" "/u/hwett/Desktop/graphics/catwalk/build/src/CMakeFiles/skinning.dir/animation_loader_saver.cc.o"
+  "/u/hwett/Desktop/graphics/catwalk/src/bone_geometry.cc" "/u/hwett/Desktop/graphics/catwalk/build/src/CMakeFiles/skinning.dir/bone_geometry.cc.o"
+  "/u/hwett/Desktop/graphics/catwalk/src/gui.cc" "/u/hwett/Desktop/graphics/catwalk/build/src/CMakeFiles/skinning.dir/gui.cc.o"
+  "/u/hwett/Desktop/graphics/catwalk/src/main.cc" "/u/hwett/Desktop/graphics/catwalk/build/src/CMakeFiles/skinning.dir/main.cc.o"
+  "/u/hwett/Desktop/graphics/catwalk/src/procedure_geometry.cc" "/u/hwett/Desktop/graphics/catwalk/build/src/CMakeFiles/skinning.dir/procedure_geometry.cc.o"
+  "/u/hwett/Desktop/graphics/catwalk/src/render_pass.cc" "/u/hwett/Desktop/graphics/catwalk/build/src/CMakeFiles/skinning.dir/render_pass.cc.o"
+  "/u/hwett/Desktop/graphics/catwalk/src/shader_uniform.cc" "/u/hwett/Desktop/graphics/catwalk/build/src/CMakeFiles/skinning.dir/shader_uniform.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "GLM_ENABLE_EXPERIMENTAL"
+  "GLM_FORCE_RADIANS=1"
+  "GLM_FORCE_SIZE_FUNC=1"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/eigen3"
+  "/usr/local/include"
+  "/opt/local/include"
+  "../lib"
+  "/lusr/opt/glfw-3.2.1/include"
+  "../lib/utgraphicsutil"
+  "../lib/pmdreader"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/u/hwett/Desktop/graphics/catwalk/build/CMakeFiles/utgraphicsutil.dir/DependInfo.cmake"
+  "/u/hwett/Desktop/graphics/catwalk/build/CMakeFiles/pmdreader.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
